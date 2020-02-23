ee230a2684b0e777dbb96842ff3e7ed2dde49d6b
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-23 14:41:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(string)
+    @word = string
+  end
+  
+  def match(string_array)
+    word_array = []
+    string_array.each do |string|
+      temp =[]
+      temp2 = []
+      temp = string.split("")
+      temp2 = @word.split("")
+      if temp.sort == temp2.sort 
+        word_array << string
+      end
+    end
+    word_array
+  end
+  
+end
